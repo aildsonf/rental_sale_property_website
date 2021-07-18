2faa0f2766d9173f8b2176f6d89bd4538c9d99e8
Aildson Ferreira <aildson.sh@gmail.com>
2021-07-18 02:07:13 -0300

    implemented image carousel

---
+++ components/Carousel.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image"
+import { useState } from "react"
+import styles from "../styles/Home.module.css"
+import { GrNext, GrPrevious } from "react-icons/gr"
+
+type Props = {
+	images: Array<string>
+}
+
+export default function Carousel({ images }: Props) {
+	const [imageNumber, setImageNumber] = useState<number>(0)
+
+	const handlePrevImage = () => {
+		return imageNumber - 1 < 0
+			? setImageNumber(0)
+			: setImageNumber(imageNumber - 1)
+	}
+	const handleNextImage = () => {
+		return imageNumber + 1 > images.length - 1
+			? setImageNumber(4)
+			: setImageNumber(imageNumber + 1)
+	}
+
+	return (
+		<>
+			<button onClick={handlePrevImage} className={styles.btn}>
+				<GrPrevious />
+			</button>
+			<Image
+				src={images[imageNumber]}
+				alt=""
+				layout="fixed"
+				height={200}
+				width={300}
+				className={styles.borderRadius}
+			/>
+			<button onClick={handleNextImage} className={styles.btn}>
+				<GrNext />
+			</button>
+		</>
+	)
+}
