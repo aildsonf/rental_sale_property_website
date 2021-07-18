ff452c81f4106c229d4fa093b56da3043b004e0e
Aildson Ferreira <aildson.sh@gmail.com>
2021-07-18 02:06:14 -0300

    added btn to trigger a modal

--- components/Card.tsx
+++ components/Card.tsx
@@ -4,15 +4,25 @@ import Image from "next/image"
 import { IProperty } from "../libs/customInterfaces"
 import formatCurrency from "../libs/formatCurrency"
 import styles from "../styles/Home.module.css"
+import ProductInfo from "./Modal"
 
-export interface Props {
+type Props = {
 	obj: IProperty
 }
 
 export default function Card({ obj }: Props) {
+	const handleModalUp = () => {
+		document.getElementById("product-info")!.style.display = "flex"
+	}
+
 	return (
 		<div id="card" className={styles.card}>
-			<Image src={obj.images[0]} alt="image" width={232} height={130} />
+			<Image
+				src={obj.images[0]}
+				alt="Foto do imóvel"
+				width={232}
+				height={130}
+			/>
 			<div className={styles.info}>
 				<b className={styles.textColor}>{`Preço ${formatCurrency(
 					obj.pricingInfos.price
@@ -24,9 +34,12 @@ export default function Card({ obj }: Props) {
 					className={styles.textColor}
 				>{`${obj.bedrooms} quarto(s) | ${obj.bathrooms} banheiro(s)`}</span>
 				<span className={styles.textColor}>{`${obj.usableAreas}m²`}</span>
+				<button id="display-info" onClick={handleModalUp}>
+					Ver Mais
+				</button>
 			</div>
 
-			<div className={styles.overlay} />
+			<ProductInfo data={obj} />
 		</div>
 	)
 }
