c381032a35fc8cdb6ce4719b711ea9bf7a331431
Aildson Ferreira <aildson.sh@gmail.com>
2021-07-18 02:05:42 -0300

    allow nextPage func to process data of any type

--- libs/pagination.ts
+++ libs/pagination.ts
@@ -13,7 +13,7 @@ export function prevPage(pageNumber: number) {
 	return pageNumber - 1 <= 0 ? 1 : pageNumber - 1
 }
 
-export function nextPage(data: Array<IProperty>, pageNumber: number) {
+export function nextPage(data: Array<IProperty | any>, pageNumber: number) {
 	let arraySize: number = data.length - 1
 	let pageSize: number = 4 // elements per page
 	let pageLimit: number = Math.floor(arraySize / pageSize) // pagination limit, max. page to be reached
