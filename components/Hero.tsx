0be097f3e48386416ef92aeef544583f757c206f
Aildson Ferreira <aildson.sh@gmail.com>
2021-07-18 02:04:21 -0300

    updated hero images

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,43 +6,34 @@ import styles from "../styles/Home.module.css"
 export default function Hero() {
 	return (
 		<>
-			<h1 className={styles.textColor}>Procurando um novo lar?</h1>
-			<div className={styles.demo}>
-				<Image
-					src={require("../assets/demo-1.jpg")}
-					alt="demo-1"
-					layout="intrinsic"
-					width={250}
-					height={350}
-				/>
-				<Image
-					src={require("../assets/demo-2.jpg")}
-					alt="demo-2"
-					layout="intrinsic"
-					width={250}
-					height={350}
-				/>
-				<Image
-					src={require("../assets/demo-3.jpg")}
-					alt="demo-3"
-					layout="intrinsic"
-					width={250}
-					height={350}
-				/>
-				<Image
-					src={require("../assets/demo-4.jpg")}
-					alt="demo-4"
-					layout="intrinsic"
-					width={250}
-					height={350}
-				/>
-				<Image
-					src={require("../assets/demo-5.jpg")}
-					alt="demo-5"
-					layout="intrinsic"
-					width={250}
-					height={350}
-				/>
+			<h1 className={`${styles.textColor} ${styles.main_title}`}>
+				Procurando um novo lar?
+			</h1>
+			<div className={styles.hero}>
+				<div className={`${styles.tip_wrapper} ${styles.tipBg1}`}>
+					<Image src={require("../assets/demo-1.jpg")} alt="teste" />
+					<p className={styles.tip_description}>teste</p>
+				</div>
+
+				<div className={`${styles.tip_wrapper} ${styles.tipBg2}`}>
+					<Image src={require("../assets/demo-2.jpg")} alt="teste" />
+					<p className={styles.tip_description}>teste</p>
+				</div>
+
+				<div className={`${styles.tip_wrapper} ${styles.tipBg3}`}>
+					<Image src={require("../assets/demo-3.jpg")} alt="teste" />
+					<p className={styles.tip_description}>teste</p>
+				</div>
+
+				<div className={`${styles.tip_wrapper} ${styles.tipBg4}`}>
+					<Image src={require("../assets/demo-4.jpg")} alt="teste" />
+					<p className={styles.tip_description}>teste</p>
+				</div>
+
+				<div className={`${styles.tip_wrapper} ${styles.tipBg5}`}>
+					<Image src={require("../assets/demo-5.jpg")} alt="teste" />
+					<p className={styles.tip_description}>teste</p>
+				</div>
 			</div>
 		</>
 	)
