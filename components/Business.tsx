9caa07f5f57980dd319b99c32f328efbbc379e9e
Aildson Ferreira <aildson.sh@gmail.com>
2021-07-19 02:14:54 -0300

    fixed sorted elements re-rendering component

--- components/Business.tsx
+++ components/Business.tsx
@@ -1,11 +1,14 @@
-import { useState } from "react"
+import dynamic from "next/dynamic"
+import React, { useState } from "react"
 import { GrNext, GrPrevious } from "react-icons/gr"
+import { maxPriceSort, minPriceSort } from "../libs/customFilters"
 
 // local imports
 import { IData, IProperty } from "../libs/customInterfaces"
 import { nextPage, prevPage } from "../libs/pagination"
 import styles from "../styles/Home.module.css"
-import Offers from "./Offers"
+// import Offers from "./Offers"
+const Offers = dynamic(() => import("../components/Offers"), { ssr: false })
 
 type Props = {
 	zap: IData
@@ -15,6 +18,7 @@ type Props = {
 export default function Business({ zap, vivaReal }: Props) {
 	const [sale, setSale] = useState<IProperty[]>([])
 	const [rent, setRent] = useState<IProperty[]>([])
+	const [key, setKey] = useState<number>(Date.now()) // "forceUpdate" alternate
 	const [salePageNumber, setSalePageNumber] = useState<number>(1)
 	const [rentPageNumber, setRentPageNumber] = useState<number>(1)
 
@@ -38,11 +42,29 @@ export default function Business({ zap, vivaReal }: Props) {
 					<h3 className={styles.textColor}>Aqui são os que estão a venda:</h3>
 					<span className={styles.filter}>
 						<p className={styles.textColor}>Ordenar: </p>
-						<a>menor preço</a>
-						<a>maior preço</a>
+						<a
+							onClick={(e) => {
+								setKey(Date.now())
+								setSale(minPriceSort(sale))
+							}}
+						>
+							menor preço
+						</a>
+						<a
+							onClick={(e) => {
+								setKey(Date.now())
+								setSale(maxPriceSort(sale))
+							}}
+						>
+							maior preço
+						</a>
 					</span>
 
-					<Offers data={sale} pageNumber={salePageNumber} />
+					<Offers
+						key={key}
+						data={sale || []}
+						pageNumber={salePageNumber || 1}
+					/>
 
 					<div className={styles.paginate}>
 						<button
@@ -63,11 +85,29 @@ export default function Business({ zap, vivaReal }: Props) {
 					<h3 className={styles.textColor}>E aqui os que estão para alugar:</h3>
 					<span className={styles.filter}>
 						<p className={styles.textColor}>Ordenar: </p>
-						<a>menor preço</a>
-						<a>maior preço</a>
+						<a
+							onClick={(e) => {
+								setKey(Date.now())
+								setRent(minPriceSort(rent))
+							}}
+						>
+							menor preço
+						</a>
+						<a
+							onClick={(e) => {
+								setKey(Date.now())
+								setRent(maxPriceSort(rent))
+							}}
+						>
+							maior preço
+						</a>
 					</span>
 
-					<Offers data={rent} pageNumber={rentPageNumber} />
+					<Offers
+						key={key}
+						data={rent || []}
+						pageNumber={rentPageNumber || 1}
+					/>
 
 					<div className={styles.paginate}>
 						<button
