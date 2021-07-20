9f48c870b1b5114a64e1f0a3451432ac760730c1
Aildson Ferreira <aildson.sh@gmail.com>
2021-07-20 06:05:40 -0300

    added title to main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,6 +15,7 @@ export default function Home({ zapData, vivarealData }: InferGetStaticPropsType<
 		<>
 			<Head>
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				<title>Code Challenge</title>
 			</Head>
 
 			<div className={styles.background} />
